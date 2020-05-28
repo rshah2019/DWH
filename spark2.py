4219f4f49de638fa1a11394b8234d8db1e2c6858
rshah2019 <53778805+rshah2019@users.noreply.github.com>
2020-05-28 16:56:55 -0400

    adds

---
+++ spark2.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+import os
+
+#os.environ["JAVA_HOME"] = "C:/Program Files/Java/jdk-11"
+#os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
+#os.environ["hadoop.home.dir"] = "C:/Users/rvs"
+
+# initialise sparkContext
+spark = SparkSession.builder \
+    .master('local') \
+    .appName('myAppName') \
+    .config('spark.executor.memory', '5gb') \
+    .config("spark.cores.max", "6") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+# using SQLContext to read parquet file
+sqlContext = SQLContext(sc)
+
+# Read in the Parquet file created above.
+# Parquet files are self-describing so the schema is preserved.
+# The result of loading a parquet file is also a DataFrame.
+parquetFile = spark.read.parquet("example.parquet")
+
+# Parquet files can also be used to create a temporary view and then used in SQL statements.
+parquetFile.createOrReplaceTempView("parquetFile")
+teenagers = spark.sql("SELECT Three from parquetFile ")
+teenagers.show()
---
+++ writer.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from hdfs import InsecureClient
+import os
+
+df = pd.DataFrame({'one': [-1, np.nan, 2.5],
+                    'two': ['foo', 'bar', 'baz'],
+                    'three': [True, False, True]},
+                  index=list('abc'))
+
+table = pa.Table.from_pandas(df)
+pq.write_table(table, 'example.parquet')
+
+table2 = pq.read_table('example.parquet')
+pd2 = table2.to_pandas()
+print(pd2)
+
+
+df = pd.read_csv("C:\\Temp\\100 Sales Records.csv")
+pdd = df.to_parquet('output.parquet')
+
+client_hdfs = InsecureClient('http://PSNYD-KAFKA-01:9870')
+#client_hdfs = InsecureClient('http://PSNYP-AFLOW-02:50070')
+f = open('output.parquet', errors='ignore')
+#client_hdfs.write('output2.parquet', f, encoding='utf-8')
+
+
+with client_hdfs.read('output2.parquet', encoding='utf-8') as reader:
+    content = reader.read()
+    print(content)
+
+df = client_hdfs.list("/user/RVS")
+print(df)
+
+#fs = pa.hdfs.connect('PSNYP-AFLOW-02', 9870, 'hdfs')
+#pq.write_to_dataset(table, root_path='dataset_name', partition_cols=['one', 'two'], filesystem=fs)
+
+
