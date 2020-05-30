e9535bbfce9c6a0e33e2940938d80f438a542630
rshah2019 <53778805+rshah2019@users.noreply.github.com>
2020-05-30 09:05:49 -0400

    docker

--- csv_reader.py
+++ csv_reader.py
@@ -1,11 +1,14 @@
 import csv
 import requests
-
+import pandas as pd
+import os
 
 TICKER_URL = 'https://raw.githubusercontent.com/plotly/dash-stock-tickers-demo-app/master/tickers.csv'
 
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def get_tickers():
+def get_tickers_web():
     ticker_map = {}
     with requests.Session() as s:
         download = s.get(TICKER_URL)
@@ -16,3 +19,9 @@ def get_tickers():
             print(l)
             ticker_map[l[1]] = l[0]
     return ticker_map
+
+
+def get_tickers():
+    path = os.path.join(__location__, 'tickers.csv')
+    df = pd.read_csv(path, sep=',')
+    return df.set_index('Symbol').to_dict()['Company']
