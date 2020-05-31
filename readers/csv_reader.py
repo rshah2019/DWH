a8ebf4f4b9296d1890f290b6cd6c826c02b92dbb
rshah2019 <53778805+rshah2019@users.noreply.github.com>
2020-05-31 11:07:31 -0400

    ccy

--- readers/csv_reader.py
+++ readers/csv_reader.py
@@ -3,22 +3,9 @@ import requests
 import pandas as pd
 import os
 
-TICKER_URL = 'https://raw.githubusercontent.com/plotly/dash-stock-tickers-demo-app/master/tickers.csv'
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-def get_tickers_web():
-    ticker_map = {}
-    with requests.Session() as s:
-        download = s.get(TICKER_URL)
-        decoded_content = download.content.decode('utf-8')
-        cr = readers.reader(decoded_content.splitlines(), delimiter=',')
-        my_list = list(cr)[1:]
-        for l in my_list:
-            print(l)
-            ticker_map[l[1]] = l[0]
-    return ticker_map
 
 
 def get_tickers():
