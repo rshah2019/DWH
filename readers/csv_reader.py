140d03e7ebc6f9346c829d51adc03689eab10a74
rshah2019 <53778805+rshah2019@users.noreply.github.com>
2020-05-31 11:12:40 -0400

    ccy

--- readers/csv_reader.py
+++ readers/csv_reader.py
@@ -12,3 +12,16 @@ def get_tickers():
     path = os.path.join(__location__, 'tickers.csv')
     df = pd.read_csv(path, sep=',')
     return df.set_index('Symbol').to_dict()['Company']
+
+
+def get_currencies():
+    path = os.path.join(__location__, 'ccy.csv')
+    df = pd.read_csv(path, sep=',')
+    return df
+
+
+def get_products():
+    path = os.path.join(__location__, 'products.csv')
+    df = pd.read_csv(path, sep=',')
+    return df
+
