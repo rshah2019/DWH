26882360614f4f4f76ed234934fa8e4021f95b3d
rshah2019 <53778805+rshah2019@users.noreply.github.com>
2020-06-02 13:39:28 -0400

    fix

--- api.py
+++ api.py
@@ -8,4 +8,4 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-app.run()
+app.run(host='0.0.0.0')
